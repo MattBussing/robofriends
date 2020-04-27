75badfea61d8d9ec8f57794a8f27cba7fe285a7a
Matt Bussing <mbussing@alarm.com>
2020-04-27 12:02:25 -0600

    saving spot

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons'
-import App from './containers/App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import "tachyons";
+import App from "./containers/App";
+import { Provider, connect } from "react-redux";
+import { createStore } from "redux";
+import { searchRobots } from "./reducers";
+
+// const store = createStore(rootReducer);
+const store = createStore(searchRobots);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
